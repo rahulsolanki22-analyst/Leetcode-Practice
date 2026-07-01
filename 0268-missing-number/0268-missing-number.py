7cd07ead9f0b90aaa26adfd15f63a0efcf7ab788
Rahul Solanki <rahulssolankii22@gmail.com>
2026-07-01 19:10:45 +0530

    Time: 0 ms (100%), Space: 13.2 MB (67.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -10,10 +10,11 @@ class Solution(object):
                 continue
             else:
                 return i"""
-        nums.sort()
+        """nums.sort()
         for i,v in enumerate(nums):
             if (i!=v):
                 return v-1
             if v==len(nums)-1:
-                return v+1
+                return v+1"""
+        return sum(range(len(nums)+1))-sum(nums)
         
